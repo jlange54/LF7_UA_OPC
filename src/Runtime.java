376fc201d53c2ab7606da35102d8badf72bab73f
jlange <jlange54@gmx.de>
2023-12-18 11:40:42 +0100

    Add runtime helper class

---
+++ src/Runtime.java
@@ -0,0 +1,24 @@
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.Duration;
+
+public class Runtime {
+
+    private static long startTime;
+    private static long endTime;
+
+    public static long currentTime () {
+        long unixTime = System.currentTimeMillis() / 1000L;
+        return unixTime;
+    }
+
+    public static void start () {
+       startTime = currentTime();
+    }
+
+    public static long end () {
+        endTime = currentTime();
+        long duration = endTime - startTime;
+        return duration;
+    }
+}
