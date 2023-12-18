c1c48251a523c192dd131cab742cf1fbfa48ff83
jlange <jlange54@gmx.de>
2023-12-18 18:55:40 +0100

    Add new call for getSensorOutputs

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import de.judge.opc_ets.Station;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       OPCClient.start();
+        Runtime.start();
+//        getAllSensors.execute();
+        getSensorOutputs.execute(Station.BF);
+        System.out.println("Program completed in " + Runtime.end() + " seconds");
+
+
     }
 }
