26b21d806ed68be79eb7e7ebfa3f4c8a2d1993fb
jlange <jlange54@gmx.de>
2024-01-29 13:55:26 +0100

    Testing

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 import Helper.Runtime;
-import de.judge.opc_ets.Station;
-
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Runtime.start();
 //        getAllSensors.execute();
-        getSensorOutputs.execute(Station.BF, 100 ,1);
-        System.out.println("Program completed in " + Runtime.end() + " seconds");
+        getSensorOutputs.execute(100 ,5000);
+        Runtime.end(Runtime.timeUnit.seconds, Runtime.type.program);
 
 
     }
