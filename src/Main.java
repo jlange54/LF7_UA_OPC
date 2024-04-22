76a4a17605a3e566ef6cf7140f604b6ce831d9a1
jlange <jlange54@gmx.de>
2024-04-22 13:46:14 +0200

    Add database class & methods to migrate data to the database

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import Helper.Runtime;
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 //        getAllSensors.execute();  // This method can be called if the saved sensor file has to be generated or updated. It is not necessary to run this everytime
-        getSensorOutputs.execute(100 ,5000);
+//        getSensorOutputs.execute(100 ,5000);
+        opcDatabase.fillStationsSensors();
     }
 }
