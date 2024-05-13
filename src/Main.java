8f4b0038ffa0dfb24c7d61575f1409dfa685878a
jlange <jlange54@gmx.de>
2024-05-13 17:25:28 +0200

    Add method call to execute getSensorOutputs

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import de.judge.opc_ets.Station;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Database.getSensorOutputs.outputsToDatabase(Station.BF ,500, 5000);
+        Database.getSensorOutputs.execute(500, 10000);
+//        Database.getSensorOutputs.outputsToDatabase(Station.BF ,500, 5000);
 //        Local.getSensorOutputs.execute(500, 5000);
+//        System.out.println(opcDatabase.getLastIdFromDataValueTable());
     }
 }
