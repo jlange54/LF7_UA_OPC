178a6842515ccb29a00bee07da4fe7597ec59f97
jlange <jlange54@gmx.de>
2024-05-13 16:23:15 +0200

    Add method to add crawl to Database

--- src/Database/getSensorOutputs.java
+++ src/Database/getSensorOutputs.java
@@ -7,32 +7,58 @@ import de.judge.opc_ets.SensorList;
 import de.judge.opc_ets.Station;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class getSensorOutputs {
 
-    public static void outputsToDatabase(Station station, SensorList sensorList, int queryInterval, int queryDuration) throws Exception {
+    public static void execute(int queryInterval, int queryDuration) throws Exception {
+        for (Station station : opcDatabase.getStationsFromDB(opcDatabase.getConnection())) {
+            if (!station.name().equals("Controller")) {
+                System.out.println("Getting output from: " + station.name());
+                outputsToDatabase(station, queryInterval, queryDuration);
+            }
+        }
+    }
+
+
+    public static void outputsToDatabase(Station station, int queryInterval, int queryDuration) throws Exception {
         OPCClientETS.getInstance().connectToMachine(station);
         OPCClientETS.getInstance().setCrawlOffset(queryInterval);
-        OPCClientETS.getInstance().browseOPCServer(sensorList);
+        OPCClientETS.getInstance().browseOPCServer(buildSensorList(station));
 
-        InputStream input = OPCClientETS.getInstance().getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        System.out.println("Building crawlList for station: " + station.name());
+        List<Crawl> crawlList = buildCrawlList(OPCClientETS.getInstance().getInputStream(), queryDuration);
+        fillCrawlTable(crawlList, station);
+
+    }
+
+    private static List<Crawl> buildCrawlList(InputStream input, int queryDuration) throws IOException, SQLException {
+        Scanner in = new Scanner(input);
         List<Crawl> crawlList = new ArrayList<>();
+        long startTime = System.currentTimeMillis();
 
-        while (reader.ready()) {
-            String line = reader.readLine();
+        while (in.hasNext() && System.currentTimeMillis() <= startTime+Long.valueOf(queryDuration)) {
+            String line = in.nextLine();
+            System.out.println(line);
+            String raw = line;
             String sensor = Regex.substitution(line, "Sensor:.*(\".*\").*VALUE:",1);
             String value = Regex.substitution(line, "DataValue.*value=(.*),.*statusCode",1);
             String sourceTimestamp = Regex.substitution(line, "sourceTimestamp=(.*),.*sourcePicoseconds",1);
             String serverTimestamp = Regex.substitution(line, "serverTimestamp=(.*),.*serverPicoseconds",1);
-            Crawl crawlLine = new Crawl(sensor, value, sourceTimestamp, serverTimestamp);
+            Crawl crawlLine = new Crawl(raw, sensor, value, sourceTimestamp, serverTimestamp);
             crawlList.add(crawlLine);
         }
+
+        opcDatabase.closeConnection(opcDatabase.getConnection());
+        return crawlList;
     }
 
     /**
@@ -49,6 +75,41 @@ public class getSensorOutputs {
             String sensorLine[] = sensor.split(";");
             sensorList.addSensor(Integer.parseInt(sensorLine[0]), sensorLine[1]);
         }
+        opcDatabase.closeConnection(opcDatabase.getConnection());
         return sensorList;
+    }
+
+    private static void fillCrawlTable (List<Crawl> crawlList, Station station) throws SQLException {
+        for (Crawl crawl : crawlList) {
+            System.out.println(crawl);
+            String datavalueSql = "INSERT INTO datavalue (RawValue, CalculatedValue, Valid) VALUES (?,?,?)";
+            try (PreparedStatement preparedStatement = opcDatabase.getConnection().prepareStatement(datavalueSql)) {
+                preparedStatement.setString(1, crawl.getRaw());
+                preparedStatement.setString(2, crawl.getValue());
+                int valid = 0;
+                if (crawl.getValue() !=" " && crawl.getValue()!=null) {
+                    valid = 1;
+                }
+                preparedStatement.setString(3, Integer.toString(valid));
+                preparedStatement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            String datacrawlSql = "INSERT INTO datacrawl (sourceTimestamp, serverTimestamp, Anlagenname, StationId, OrderId, DataValueId, SensorId) VALUES (?,?,?,?,?,?,?)";
+            try (PreparedStatement preparedStatement = opcDatabase.getConnection().prepareStatement(datacrawlSql)) {
+                preparedStatement.setString(1, crawl.getSourceTimestamp());
+                preparedStatement.setString(2, crawl.getServerTimestamp());
+                preparedStatement.setString(3, "LF7");
+                preparedStatement.setString(4, Integer.toString(opcDatabase.getStationIdToSensor(crawl.getSensor())));
+                preparedStatement.setString(5, "-1");
+                preparedStatement.setString(6, Integer.toString(opcDatabase.getLastIdFromDataValueTable()));
+                preparedStatement.setString(7, Integer.toString(opcDatabase.getSensorId(crawl.getSensor())));
+                preparedStatement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            opcDatabase.closeConnection(opcDatabase.getConnection());
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-import Database.migration;
 import Database.opcDatabase;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
+import de.judge.opc_ets.Station;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        Local.getAllSensors.execute();  // This method can be called if the saved sensor file has to be generated or updated. It is not necessary to run this everytime
-//        Local.getSensorOutputs.execute(100 ,5000);
-        migration.fillStationsSensors();
+        Database.getSensorOutputs.outputsToDatabase(Station.BF ,500, 5000);
+//        Local.getSensorOutputs.execute(500, 5000);
     }
 }
