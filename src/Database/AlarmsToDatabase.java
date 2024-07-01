7d8d4c2012e7189a3c0e05da080600db0b3ef966
jlange <jlange54@gmx.de>
2024-07-01 08:44:02 +0200

    Extract SQL into own method

--- src/Database/AlarmsToDatabase.java
+++ src/Database/AlarmsToDatabase.java
@@ -13,18 +13,22 @@ public class AlarmsToDatabase {
        AlarmMainStructure mainStructure = AlarmMainStructure.mapFromJson(ApiConnector.getAllAlarms().body());
        for (AlarmMainStructure.Alarm alarm : mainStructure.getAlarms()) {
            for (AlarmMainStructure.Alarm.Error error : alarm.getErrors()) {
-               String alarmSql = "Insert INTO alarm (StationId, fehlerCode, fehlerMsg, anlagenId) VALUES (?, ?, ?, ?)";
-               try (PreparedStatement preparedStatement = OpcDatabase.getConnection().prepareStatement(alarmSql)) {
-                   preparedStatement.setInt(1, OpcDatabase.getStationIdFromName(alarm.getModuleId()));
-                   preparedStatement.setInt(2, error.getCode());
-                   preparedStatement.setString(3, error.getMsg());
-                   preparedStatement.setInt(4,1);
-
-                   preparedStatement.execute();
-               } catch (SQLException e) {
-                   e.printStackTrace();
-               }
+              insertAlarmToDatabase(alarm.getModuleId(), error.getCode(), error.getMsg(), 1);
            }
        }
+    }
+
+    public static void insertAlarmToDatabase(String stationId, int fehlerCode, String fehlerMsg, int anlagenId) throws IOException, InterruptedException {
+        String alarmSql = "Insert INTO alarm (StationId, fehlerCode, fehlerMsg, anlagenId) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = OpcDatabase.getConnection().prepareStatement(alarmSql)) {
+            preparedStatement.setInt(1, OpcDatabase.getStationIdFromName(stationId));
+            preparedStatement.setInt(2, fehlerCode);
+            preparedStatement.setString(3, fehlerMsg);
+            preparedStatement.setInt(4,anlagenId);
+
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
